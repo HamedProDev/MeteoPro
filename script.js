9a08d3293793ae40c92d801137c86bd9bfc2b073
HamedProDev <hamussein01@gmail.com>
2026-09-07 19:22:06 +0200

    added apiKey and  apiUrl  variables

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-cons
+const apiKey = "5f2d0f6509149b6e45a0f18e62d97c7a";
+const  apiUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=kigali";
+
+const  checkWeather =async ()=>{
+    const response =  await fetch(apiUrl+ `&appid=${apiKey}`)
+    var data = await response.json();
+
+    console.log(data)
+}
+checkWeather()
